fbc77206d105210b07e79ff7ce3ceeb6b4781138
chrisesbueno <alvarezchristopherve@gmail.com>
2022-10-25 21:00:34 -0400

    Proyecto News Homepage: Commit 01

---
+++ main.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('body');
+const menu = document.querySelector('.icon-menu')
+const close = document.querySelector('.icon-close')
+
+menu.addEventListener('click', () => {
+    header.classList.add('active')
+})
+
+close.addEventListener('click', () => {
+    header.classList.remove('active')
+})
